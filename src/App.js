bd7d6d86ed0be29217473701f4084e60264cc862
mmatos394 <126423898+mmatos394@users.noreply.github.com>
2023-03-16 09:32:59 +0000

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
         <h1>Weather App</h1>
         <SearchForm />
       </header>
+      <footer>
+        This project was coded by <strong>Melissa Matos</strong> and is
+        <a href="https://github.com/mmatos394/Weather-App">
+          open-sourced on GitHub.
+        </a>
+      </footer>
     </div>
   );
 }
